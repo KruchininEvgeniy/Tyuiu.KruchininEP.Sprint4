59211b1e8c428c03525f52848f88d4e50737b07d
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-11-14 21:12:03 +0500

    Task6

---
+++ Tyuiu.KruchininEP.Sprint4.Task6.V6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KruchininEP.Sprint4.Task6.V6.Lib;
+
+namespace Tyuiu.KruchininEP.Sprint4.Task6.V6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Кручинин Е. П. | РППб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Класс Array                                                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Кручинин Е. П. | РППб25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан строковый массив данных [Борис, Анна, Михаил, Ирина, Сергей,        *");
+            Console.WriteLine("* Татьяна, Олег], используя класс Array, выведите элементы массива,       *");
+            Console.WriteLine("* длина которых равна 5 символам.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string[] name = { "Борис", "Анна", "Михаил", "Ирина", "Сергей", "Татьяна", "Олег" };
+
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write($"{name[i]} ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Элементы массива, длина которых равна 5 символов: ");
+
+            string[] res = ds.Calculate(name);
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
