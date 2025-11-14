05551253cb95c6526be5e55ecc14449e72dc70ad
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-11-14 21:07:30 +0500

    Task5

---
+++ Tyuiu.KruchininEP.Sprint4.Task5.V8/Program.cs
@@ -0,0 +1,67 @@
+﻿using Tyuiu.KruchininEP.Sprint4.Task5.V8.Lib;
+
+namespace Tyuiu.KruchininEP.Sprint4.Task5.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Кручинин Е. П. | РППб25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Кручинин Е. П. | РППб25-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от -3 до 4.                           *");
+            Console.WriteLine("* Заменить отрицательные элементы на 0.                                   *");
+            Console.WriteLine("***************************************************************************");
+            DataService dataService = new DataService();
+            Random rand = new Random();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.Write("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество столбцов в массиве: ");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[rows, cols];
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rand.Next(-3, 4);
+                }
+            }
+            Console.WriteLine("\nМассив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Новый массив:");
+            int[,] newMas = dataService.Calculate(matrix);
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{matrix[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
