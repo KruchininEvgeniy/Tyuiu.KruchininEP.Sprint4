2c1c4da09148d4e6c787b8d58080ac39606baf76
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-11-14 19:12:42 +0500

    Task0

--- Tyuiu.KruchininEP.Sprint4.Task0.V4.Lib/DataService.cs
+++ Tyuiu.KruchininEP.Sprint4.Task0.V4.Lib/DataService.cs
@@ -1,30 +1,20 @@
-﻿using System;
-using tyuiu.cources.programming.interfaces.Sprint4;
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
 
-namespace Tyuiu.KruchininEP.Sprint4.Task0.V4.Lib
+namespace Tyuiu.MiliukovLO.Sprint4.Task0.V4.Lib
 {
     public class DataService : ISprint4Task0V4
     {
         public int GetMultEvenArrEl(int[] array)
         {
-            int mult = 1;
-            bool hasEven = false;
-
+            int result = 1;
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] % 2 == 0)
                 {
-                    mult *= array[i];
-                    hasEven = true;
+                    result *= array[i];
                 }
             }
-
-            if (!hasEven)
-            {
-                mult = 0;
-            }
-
-            return mult;
+            return result;
         }
     }
 }
