997a4260e5271b285d51dbccb302c8f223a43aa5
Евгений Кручинин <Tyuiu.KruchininEP@outlook.com>
2025-11-14 21:43:36 +0500

    Task3

--- Tyuiu.KruchininEP.Sprint4.Task3.V2.Test/DataServiceTest.cs
+++ Tyuiu.KruchininEP.Sprint4.Task3.V2.Test/DataServiceTest.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Tyuiu.KruchininEP.Sprint4.Task3.V2.Lib;
-
-namespace Tyuiu.KruchininEP.Sprint4.Task3.V2.Test
+﻿namespace Tyuiu.KruchininEP.Sprint4.Task3.V2.Test
 {
     [TestClass]
     public sealed class DataServiceTest
@@ -9,20 +6,6 @@ namespace Tyuiu.KruchininEP.Sprint4.Task3.V2.Test
         [TestMethod]
         public void TestMethod1()
         {
-
-            int[,] array = new int[,]
-            {
-                {4, 3, 2, 8, 7},
-                {4, 6, 5, 5, 7},
-                {3, 8, 8, 3, 5},
-                {5, 7, 5, 4, 4},
-                {8, 8, 3, 7, 6}
-            };
-
-            DataService ds = new DataService();
-            int result = ds.GetMaxInFirstRow(array);
-
-            Assert.AreEqual(8, result); 
         }
     }
 }
